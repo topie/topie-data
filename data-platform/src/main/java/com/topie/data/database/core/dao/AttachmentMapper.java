fc8d22074358800f16fdd7cd0c6dc7e57694748b
chenJz1012 <597160667@qq.com>
2017-09-11 17:29:39 +0800

    init

---
+++ data-platform/src/main/java/com/topie/data/database/core/dao/AttachmentMapper.java
@@ -0,0 +1,7 @@
+package com.topie.data.database.core.dao;
+
+import com.topie.data.database.core.model.Attachment;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface AttachmentMapper extends Mapper<Attachment> {
+}
